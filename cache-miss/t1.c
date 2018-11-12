f6692d50163c3a59ea81c5d3d9de239769d4a931
zhenwei pi <pizhenwei@bytedance.com>
2018-11-12 16:03:36 +0800

    Update cache miss case

--- cache-miss/t1.c
+++ cache-miss/t1.c
@@ -1,19 +1,10 @@
+#include <stdlib.h>
+#define MISS 39321600
 
-#define NUM 393216
- 
 int main(){
-    float a[NUM],b[NUM];
+    int *p = (int*)malloc(MISS * sizeof(int));
     int i;
-    for(i=0;i<1000;i++)
-        add(a,b,NUM);
+    for(i=0; i<MISS; i++) {
+        p[random() % MISS] = 1;
+    }
 }
- 
-int add(int *a,int *b,int num){
-    int i=0;
-    for(i=0;i<num;i++){
-        *a=*a+*b;
-        a++;
-        b++;
-    }   
-}
-
--- cache-miss/t2.c
+++ cache-miss/t2.c
@@ -1,20 +1,10 @@
-#define NUM 39216
-typedef struct{
-    float a;
-    float b;
-}Array;
- 
+#include <stdlib.h>
+#define MISS 39321600
+
 int main(){
-    Array myarray[NUM];
-    int j=0;
-    for(j=0;j<1000;j++)
-        add(myarray,NUM);
-}
- 
-int add(Array *myarray,int num){
-    int i=0;
-    for(i=0;i<num;i++){
-        myarray->a=myarray->a+myarray->b;
-        myarray++;
-    }   
+    int *p = (int*)malloc(MISS * sizeof(int));
+    int i;
+    for(i=0; i<MISS; i++) {
+        p[i] = 1;
+    }
 }
